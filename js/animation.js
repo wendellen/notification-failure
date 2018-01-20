99de6e17a304a521d398632f6ec38ba15291ef60
Crystal Emo <wendellenli@gmail.com>
2018-01-20 13:23:34 -0800

    adds seperate class and function for inverted notifications

--- js/animation.js
+++ js/animation.js
@@ -18,13 +18,13 @@ var assets = ['0.png', '1.png', '2.png', '3.png', '5.png', '6.png']
 
 function makeNotification(){
 
-
   $("<div class='notification'>")
     .css({
       left: window.innerWidth*Math.random()-200,
       top: window.innerHeight*Math.random()-200,
       'background-image': "url(assets/" + assets[assetIndex % assets.length] + ")",
       'background-repeat': 'no-repeat'
+
     })
     .appendTo("body")
     .animateCss('zoomIn')
@@ -33,46 +33,91 @@ function makeNotification(){
 
 }
 
-function popUp(){
-  var makeTimer = setInterval(makeNotification, 117.646875)
+function makeInverted(){
+
+  $("<div class='notificationInverted'>")
+    .css({
+      left: window.innerWidth*Math.random()-200,
+      top: window.innerHeight*Math.random()-200,
+      'background-image': "url(assets/" + assets[assetIndex % assets.length] + ")",
+      'background-repeat': 'no-repeat',
+      'filter': 'invert(100%)',
+    })
+    .appendTo("body")
+    .animateCss('zoomIn')
+
+    assetIndex += 1
+
+}
+
+function intro(){
+  var introTimer = setInterval(makeNotification, 472*8)
   setTimeout( function(){
-    clearInterval( makeTimer )
+    clearInterval( introTimer )
 
-    setTimeout(changeColor, 5)
+    setTimeout(popUp, 1)
 
-  }, 50000)
+  }, 30000)
+}
+
+function popUp(){
+  var popTimer = setInterval(makeNotification, 472*4)
+  setTimeout( function(){
+    clearInterval( popTimer )
+
+    setTimeout(popUp2, 1)
+
+  }, 30000)
+}
+
+function popUp2(){
+  var popTimer = setInterval(makeNotification, 472/4)
+  setTimeout( function(){
+    clearInterval( popTimer )
+
+    setTimeout(changeColor, 1)
+
+  }, 22000)
 }
 
 function changeColor(){
-  // $('.notification').css('filter', 'hue-rotate(90deg)')
-  var colorTimer = setInterval(function(){
-    var notifications = $('.notification')
-    var notification = notifications.eq( Math.round(Math.random()* (notifications.length-1)) )
-    notification.css('filter', 'invert(100%)')
-    notification.css('transform', 'rotate(180deg)')
-  }, 88.23515625)
+
+  var colorTimer = setInterval(makeInverted, 472)
 
   setTimeout( function(){
     clearInterval( colorTimer )
-    goBlack()
-  }, 85000)
 
-  // $('.notification').css('filter', 'brightness(0)')
+    setTimeout(rotate, 1)
+
+  }, 53000)
+}
+
+function rotate(){
+  var rotateTimer = setInterval(function(){
+    var notifications = $('.notificationInverted')
+    var notification = notifications.eq( Math.round(Math.random()* (notifications.length-1)) )
+    notification.css('transform', 'rotate(180deg)')
+  }, 472)
+
+  setTimeout( function(){
+    clearInterval( rotateTimer )
+    setTimeout( goBlack, 1)
+  }, 30000)
 }
 
 function goBlack(){
   // $('.notification').css('filter', 'hue-rotate(90deg)')
   var blackTimer = setInterval(function(){
-    var notifications = $('.notification')
+    var notifications = $('.notificationInverted')
     var notification = notifications.eq( Math.round(Math.random()* (notifications.length-1)) )
     notification.css('filter', 'brightness(0)')
-  }, 4)
+  }, 472/32)
 
   setTimeout( function(){
     clearInterval( blackTimer )
-  }, 30000)
+  }, 31000)
 
   // $('.notification').css('filter', 'brightness(0)')
 }
 
-popUp()
+intro()
